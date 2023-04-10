5891bc726e08ce99eb55e8ea14932f70f44c59d9
Stefan <stefanstaxbusiness@gmail.com>
2023-04-10 18:11:33 +0200

    Step one of the app finalized

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ const App = () => {
     setBooks(updatedBooks);
   };
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
@@ -23,7 +32,7 @@ const App = () => {
   return (
     <div>
       <h1>Below find all books:</h1>
-      <BookList books={books} onDelete={deleteBookById} />
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,10 @@
 import BookShow from "./BookShow";
 
-const BookList = ({ books, onDelete }) => {
+const BookList = ({ books, onDelete, onEdit }) => {
   const renderedBooks = books.map((book) => {
-    return <BookShow book={book} onDelete={onDelete} key={book.id} />;
+    return (
+      <BookShow book={book} onDelete={onDelete} onEdit={onEdit} key={book.id} />
+    );
   });
   return <div className="book-list">{renderedBooks}</div>;
 };
