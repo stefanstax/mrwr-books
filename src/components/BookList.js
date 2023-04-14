39689e4d8c248b1e9e529031f2c5eaac64494698
Stefan <stefanstaxbusiness@gmail.com>
2023-04-14 10:52:18 +0200

    Context progress

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,19 @@
+import { useContext } from "react";
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const num = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
     return (
       <BookShow book={book} onDelete={onDelete} onEdit={onEdit} key={book.id} />
     );
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {num} {renderedBooks}
+    </div>
+  );
 };
 
 export default BookList;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import BooksContext from "./context/books";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+);
